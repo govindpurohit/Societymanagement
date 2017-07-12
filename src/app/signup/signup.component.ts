b45ad7406c29466bf6dd5c555ad5aa29df23dcbd
govindsinh.purohit <govindsinh.purohit@solutionanalysts.com>
2017-07-12 12:40:08 +0530

    no message

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -13,6 +13,7 @@ export class SignupComponent implements OnInit {
    @ViewChild('f') signUpForm:NgForm;
    hasError:boolean = false;
     errorMessage:String = "";
+    flatNumber:Array <Number>= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
   constructor(private loginService:LoginService,private route:Router) { }
 
